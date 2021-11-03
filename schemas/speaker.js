1aede28465d5d82a808475c7f58f55176e3a0274
Rodrigo Reyes <rsanchez@vectorworks.net>
2021-11-03 13:57:20 -0400

    And so it begins

---
+++ schemas/speaker.js
@@ -0,0 +1,34 @@
+export default {
+    name: 'speaker',
+    type: 'document',
+    title: 'Speaker',
+    fields: [
+      {
+        name:'firstName',
+        type: 'string',
+        title: 'Name',
+      },
+      {
+        name:'lastName',
+        type: 'string',
+        title: 'Last Name',
+      },
+      {
+        name:'company',
+        type: 'string',
+        title: 'Company',
+      },
+      {
+        name:'role',
+        type: 'string',
+        title: 'Role',
+      },
+      {
+        name:'bio',
+        type: 'text',
+        title: 'Bio',
+      },
+      
+
+    ]
+  }
